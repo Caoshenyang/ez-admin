6999d78a601e87e82d760f4653c2125cbc790b90
曹申阳 <13003016790@163.com>
2025-03-13 10:50:40 +0800

    feat: 项目初始化

---
+++ src/main/java/com/ezadmin/EzAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ezadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezadmin/EzAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
