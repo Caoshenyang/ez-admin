aae4b621a715c19054804b74cc1a123d240beb2e
Caoshenyang <2417254000@qq.com>
2025-03-17 15:21:42 +0800

    feat: 1.添加mybatis-plus分页插件，防止全表更新插件 2. 设置自动填充创建时间和更新时间字段

--- src/main/java/com/ezadmin/EzAdminApplication.java
+++ src/main/java/com/ezadmin/EzAdminApplication.java
@@ -1,9 +1,7 @@
 package com.ezadmin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("com.ezadmin.modules.**.mapper")
 @SpringBootApplication
 public class EzAdminApplication {
 
---
+++ src/main/java/com/ezadmin/common/handler/MyMetaObjectHandler.java
@@ -0,0 +1,34 @@
+package com.ezadmin.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ *  MyBatis-Plus自动填充
+ * </p>
+ *
+ * @author 曹申阳
+ * @since 2024-10-12 15:47:48
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
