eed54efe4c3036d88d35b904f8234230634eb6aa
Caoshenyang <2417254000@qq.com>
2025-03-22 21:17:11 +0800

    feat: 补充注释

--- src/main/java/com/ezadmin/service/DeptManagementService.java
+++ src/main/java/com/ezadmin/service/DeptManagementService.java
@@ -28,32 +28,57 @@ public class DeptManagementService {
 
     private final IDeptService deptService;
 
+    /**
+     * 部门创建
+     * @param deptCreateDTO deptCreateDTO
+     */
     public void createDept(DeptCreateDTO deptCreateDTO) {
         // todo 一些校验
         Dept dept = MsDeptMapper.INSTANCE.deptCreateDTO2Dept(deptCreateDTO);
         deptService.save(dept);
     }
 
+    /**
+     * 部门更新
+     * @param deptUpdateDTO deptUpdateDTO
+     */
     public void updateDept(DeptUpdateDTO deptUpdateDTO) {
         Dept dept = MsDeptMapper.INSTANCE.deptUpdateDTO2Dept(deptUpdateDTO);
         deptService.updateById(dept);
 
     }
 
+    /**
+     * 根据ID删除部门
+     * @param deptId deptId
+     */
     public void deleteDept(Long deptId) {
         deptService.removeById(deptId);
     }
 
+    /**
+     * 批量删除部门
+     * @param deptIdList deptIdList
+     */
     public void deleteBatchDept(List<Long> deptIdList) {
         deptService.removeBatchByIds(deptIdList);
     }
 
+    /**
+     * 根据ID查询部门详情
+     * @param deptId deptId
+     * @return DeptDetailVO
+     */
     public DeptDetailVO getDeptById(Long deptId) {
         return Optional.ofNullable(deptService.getById(deptId))
                 .map(MsDeptMapper.INSTANCE::dept2DeptDetailDTO)
                 .orElse(null);
     }
 
+    /**
+     * 查询部门树
+     * @return List<DeptTreeVO>
+     */
     public List<DeptTreeVO> findDeptTree() {
         return Optional.ofNullable(deptService.list())
                 .filter(CollectionUtils::isNotEmpty)
